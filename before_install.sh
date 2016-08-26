ea7bc3a2297f4c509483504a732421859351c8b4
CrikeeIP <noig@arcor.de>
2016-08-26 15:41:35 +0200

    travis

--- before_install.sh
+++ before_install.sh
@@ -8,3 +8,20 @@ mkdir build && cd build
 cmake ..
 make -j4 && sudo make install && cd ../..
 
+
+mkdir dependencies
+cd dependencies
+mkdir include
+cd include
+wget https://raw.githubusercontent.com/dtschump/CImg/master/CImg.h
+wget https://raw.githubusercontent.com/CrikeeIP/Geometry/master/include/geometry/geometry.h
+cd ..
+
+git clone https://github.com/Dobiasd/FunctionalPlus
+cd FunctionalPlus
+mkdir build
+cd build
+cmake ..
+sudo make install
+cd ..
+cd ..
