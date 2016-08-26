1de2d898e1b383e15c3c48007d3d757d43518c72
CrikeeIP <noig@arcor.de>
2016-08-26 15:22:24 +0200

    travis

--- before_install.sh
+++ before_install.sh
@@ -1,4 +1,4 @@
-echo ----- google test start ----
+
 export GTEST_VERSION=master
 export GTEST=googletest-${GTEST_VERSION}
 wget https://github.com/google/googletest/archive/${GTEST_VERSION}.tar.gz
@@ -7,29 +7,4 @@ cd ${GTEST}
 mkdir build && cd build
 cmake ..
 make -j4 && sudo make install && cd ../..
-echo ----- google test end ----
 
-echo ----- 1 ----
-mkdir dependencies
-echo ----- 2 ----
-cd dependencies
-echo ----- 3 ----
-mkdir include
-echo ----- 4 ----
-cd include
-echo ----- 5 ----
-wget https://raw.githubusercontent.com/dtschump/CImg/master/CImg.h
-wget https://raw.githubusercontent.com/CrikeeIP/Geometry/master/include/geometry/geometry.h
-cd ..
-echo ----- 6 ----
-
-git clone https://github.com/Dobiasd/FunctionalPlus
-cd FunctionalPlus
-mkdir build
-cd build
-cmake ..
-sudo make install
-cd ..
-cd ..
-
-echo ----- 7 ----
