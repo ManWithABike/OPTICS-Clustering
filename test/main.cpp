400a2743c524572b23c050a98c8d32e18c2427f3
CrikeeIP <noig@arcor.de>
2016-08-26 10:59:04 +0200

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -4,6 +4,10 @@ typedef std::vector<double> point; //A list of n cartesian coordinates makes a p
 std::vector<point> points; //Your list of points goes here
 
 int main(){
+   points = { {100,100}, {102,100}, {101,101},           //cluster 1
+               {-1,0}, {1,0}, {0,1},                     //cluster 2
+               {-100,-100}, {-102,-100}, {-101,-101}     //cluster 3
+   };
    auto reach_dists = optics::compute_reachability_dists( points, 10, 100 );
    optics::draw_reachability_plot( reach_dists, "D:/reachdists.bmp" );
 }
