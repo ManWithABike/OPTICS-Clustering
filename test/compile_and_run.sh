a629964dcb35744691798162824d972c4d32813d
CrikeeIP <noig@arcor.de>
2017-06-07 14:23:49 +0200

    Version Control

--- test/compile_and_run.sh
+++ test/compile_and_run.sh
@@ -1,8 +1,9 @@
-rm run
-rm reachdists.bmp
+rm -f run
+rm -f reachdists.bmp
 
 export CC=/usr/bin/gcc-6
 export CXX=/usr/bin/g++-6
-
+gcc -v && g++ -v && cmake --version
+ 
 g++ -std=c++11 -g test_main.cpp -o run -lX11 -pthread
 ./run
