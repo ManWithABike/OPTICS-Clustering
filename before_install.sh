eb25d2204ba5939df39b40cbfc16b679423915de
CrikeeIP <noig@arcor.de>
2017-06-07 14:00:32 +0200

    export CC / CXX

--- before_install.sh
+++ before_install.sh
@@ -5,10 +5,10 @@ cd dependencies
 	cd include
 	sudo mkdir /usr/local/include/CImg
 	sudo wget https://raw.githubusercontent.com/dtschump/CImg/master/CImg.h -O /usr/local/include/CImg/CImg.h
-	echo "--------- CImg downloaded ---------\n"
+	printf "--------- CImg downloaded ---------\n"
 	sudo mkdir /usr/local/include/geometry
 	sudo wget https://raw.githubusercontent.com/CrikeeIP/Geometry/master/include/geometry/geometry.hpp -O /usr/local/include/geometry/geometry.hpp
-	echo "--------- Geometry downloaded ---------\n"
+	printf "--------- Geometry downloaded ---------\n"
 	cd ..
 
 	git clone https://github.com/Dobiasd/FunctionalPlus
@@ -19,7 +19,7 @@ cd dependencies
 	sudo make install
 	cd ..
 	cd ..
-	echo "--------- Fplus installed ---------\n"
+	printf "--------- Fplus installed ---------\n"
 
 cd ..
 sudo cp -r ./include/optics /usr/local/include/
--- test/compile_and_run.sh
+++ test/compile_and_run.sh
@@ -1,4 +1,8 @@
 rm run
 rm reachdists.bmp
+
+export CC=/usr/bin/gcc-6
+export CXX=/usr/bin/g++-6
+
 g++ -std=c++11 -g test_main.cpp -o run -lX11 -pthread
 ./run
