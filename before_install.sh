95dbaf90799aac8c134663f69c2841e07e5e4385
CrikeeIP <noig@arcor.de>
2017-06-07 10:27:47 +0200

    befor_install fixed

--- before_install.sh
+++ before_install.sh
@@ -5,10 +5,10 @@ cd dependencies
 	cd include
 	sudo mkdir /usr/local/include/CImg
 	sudo wget https://raw.githubusercontent.com/dtschump/CImg/master/CImg.h -O /usr/local/include/CImg/CImg.h
-	echo "CImg downloaded"
+	echo "--------- CImg downloaded ---------\n"
 	sudo mkdir /usr/local/include/geometry
-	sudo wget https://raw.githubusercontent.com/CrikeeIP/Geometry/master/include/geometry/geometry.h -O /usr/local/include/geometry/geometry.h
-	echo "Geometry downloaded"
+	sudo wget https://raw.githubusercontent.com/CrikeeIP/Geometry/master/include/geometry/geometry.hpp -O /usr/local/include/geometry/geometry.hpp
+	echo "--------- Geometry downloaded ---------\n"
 	cd ..
 
 	git clone https://github.com/Dobiasd/FunctionalPlus
@@ -19,13 +19,14 @@ cd dependencies
 	sudo make install
 	cd ..
 	cd ..
-	echo "Fplus installed"
+	echo "--------- Fplus installed ---------\n"
 
 cd ..
+cp -r ./include /usr/local/include/
 
 	cd test
 	chmod +x ./compile_and_run.sh
 	./compile_and_run.sh
-	echo "Tests successfully completed"
+	echo "--------- Tests successfully completed ---------"
 
 cd..
