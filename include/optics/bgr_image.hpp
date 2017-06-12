88e4848d477b4397c815c2bdd85e3376d3f0fd98
CrikeeIP <noig@arcor.de>
2017-06-12 13:15:01 +0200

    #include <stdexcept> in bgr_image

--- include/optics/bgr_image.hpp
+++ include/optics/bgr_image.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <stdexcept>
 
 struct bgr_col
 {
--- include/optics/optics.h
+++ include/optics/optics.h
@@ -20,7 +20,6 @@
 
 #include <vector>
 #include <exception>
-#include <stdexcept>
 
 
 
